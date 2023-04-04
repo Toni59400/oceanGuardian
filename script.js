1cbb2da7d6a4350a40b78ceb43e26211d91159ce
Toni59400 <95470541+Toni59400@users.noreply.github.com>
2023-04-04 17:52:23 +0200

    r

--- script.js
+++ script.js
@@ -29,8 +29,49 @@ function gClick(){
 }
 
 g.onclick = gClick;
-
-
-
-
 setInterval(dClick, 3000)
+
+
+var nb1 = document.getElementById("nb1");
+var nb2 = document.getElementById("nb2");
+var nb3 = document.getElementById("nb3");
+var txt1 = document.getElementById("txt1");
+var txt2 = document.getElementById("txt2");
+var txt3 = document.getElementById("txt3");
+
+nb1.onclick = function(){
+    nb1.classList.add("select");
+    nb1.classList.remove("bg-gris")
+    nb2.classList.remove("select")
+    nb3.classList.remove("select")
+    nb2.classList.add("bg-gris")
+    nb3.classList.add("bg-gris")
+    txt1.style.display = 'block';
+    txt2.style.display = "none";
+    txt3.style.display = "none";
+}
+
+nb2.onclick = function(){
+    nb2.classList.add("select");
+    nb2.classList.remove("bg-gris")
+    nb1.classList.remove("select")
+    nb3.classList.remove("select")
+    nb1.classList.add("bg-gris")
+    nb3.classList.add("bg-gris")
+    txt2.style.display = 'block';
+    txt1.style.display = "none";
+    txt3.style.display = "none";
+}
+
+nb3.onclick = function(){
+    nb3.classList.add("select");
+    nb3.classList.remove("bg-gris")
+    nb2.classList.remove("select")
+    nb1.classList.remove("select")
+    nb2.classList.add("bg-gris")
+    nb1.classList.add("bg-gris")
+    txt3.style.display = 'block';
+    txt1.style.display = "none";
+    txt2.style.display = "none";
+}
+
